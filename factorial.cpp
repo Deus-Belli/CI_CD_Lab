b1f726991e6a6fbb9005dabb228901c83f3d7843
Vitalii Kolkovskyi <33636751+Deus-Belli@users.noreply.github.com>
2021-05-27 12:06:26 +0300

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,11 +1,12 @@
 #include "factorial.hpp"
+#include "user_exception.hpp"
 
 int factorial(int n)
 {
  if(n<0)
-  return -1;
+  throw UserException{};
  if(n == 0)
   return 1;
-
+ 
  return n * factorial(n-1);
 }
