04b305bd6d9bbac39140f7f32ba0b8bc897f6b44
Vitalii Kolkovskyi <33636751+Deus-Belli@users.noreply.github.com>
2021-05-27 12:09:11 +0300

    Update factorialTest.cpp

--- factorialTest.cpp
+++ factorialTest.cpp
@@ -1,24 +1,27 @@
 #include "factorial.hpp"
 #include "gtest/gtest.h"
+#include "user_exception.hpp"
 
 TEST(factorialTest, HandlesZeroInput) {
-  EXPECT_EQ(factorial(0), 1);
+ EXPECT_EQ(factorial(0), 1);
 }
 
 // Tests factorial of positive numbers.
-TEST(factorialTest, HandlesPositiveInput) {
-   EXPECT_EQ(factorial(1), 1);
-   EXPECT_EQ(factorial(2), 2);
-   EXPECT_EQ(factorial(3), 6);
-   EXPECT_EQ(factorial(8), 40320);
+TEST(factorialTest, HandlesPositiveInput) 
+{
+ EXPECT_EQ(factorial(1), 1);
+ EXPECT_EQ(factorial(2), 2);
+ EXPECT_EQ(factorial(3), 6);
+ EXPECT_EQ(factorial(8), 40320);
 }
 
 TEST(factorialTest, HandleNegativeInput) {
-   EXPECT_EQ(factorial(-10), -1);
+ EXPECT_THROW(factorial(-10), UserException);
 }
 
 int main(int argc, char **argv)
 {
-   testing::InitGoogleTest(&argc, argv);
-   return RUN_ALL_TESTS();
+ testing::InitGoogleTest(&argc, argv);
+ return RUN_ALL_TESTS();
 }
+
